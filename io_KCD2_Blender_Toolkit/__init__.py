125579e52194dfac95f3b3e9823fcfd6eb38e474
LuneMods <LuneMods@sudomail.com>
2025-02-22 21:56:15 +1000

    Refractoring

--- io_KCD2_Blender_Toolkit/__init__.py
+++ io_KCD2_Blender_Toolkit/__init__.py
@@ -10,63 +10,38 @@ bl_info = {
 
 import bpy
 from bpy_extras.io_utils import ImportHelper
-from bpy.props import StringProperty
-from . import collada_handler, skin_handler
+from bpy.types import AddonPreferences, PropertyGroup
+from bpy.props import StringProperty, IntProperty, FloatProperty, EnumProperty
+from . import importers, ui
 
-class ImportKCD2ColladaOperator(bpy.types.Operator, ImportHelper):
-    """Import KCD2 Collada"""
-    bl_idname = "import_scene.kcd2_collada"
-    bl_label = "Import KCD2 COLLADA file (.dae)"
-    bl_options = {'REGISTER', 'UNDO'}
-    
-    filename_ext = ".dae"
-    filter_glob: StringProperty(default="*.dae", options={'HIDDEN'}, maxlen=255)
+class AddonSettings(AddonPreferences):
+    bl_idname = __name__
 
-    def execute(self, context):
-        return collada_handler.import_collada(self.filepath, context, self)
+    filepath: StringProperty(
+        name="KCD2 Data Directory",
+        description="The folder of the extracted .paks",
+        subtype='FILE_PATH',
+        default=""
+    )
 
-class ImportKCD2SkinOperator(bpy.types.Operator, ImportHelper):
-    """Import KCD2 Skin"""
-    bl_idname = "import_scene.kcd2_skin"
-    bl_label = "Import KCD2 Skin file (.skin)"
-    bl_options = {'REGISTER', 'UNDO'}
-    
-    filename_ext = ".skin"
-    filter_glob: StringProperty(default="*.skin", options={'HIDDEN'}, maxlen=255)
+    def draw(self, context):
+        layout = self.layout
+        layout.prop(self, "filepath")
 
-    def execute(self, context):
-        skin_filepath = self.filepath
-
-        try:
-            #convert the skin to dae
-            dae_filepath = skin_handler.skin_to_dae(skin_filepath)
-            self.report({'INFO'}, "Converting Skin to dae...")
-            if not dae_filepath:
-                raise Exception("Failed to Convert Skin to dae")
-            
-            #then import the dae
-            collada_handler.import_collada(dae_filepath, context, self)
-            self.report({'INFO'}, "Model imported successfully.")
-            
-        except Exception as e:
-            self.report({'ERROR'}, f"Import failed: {e}")
-            return {'CANCELLED'}
-
-        return {'FINISHED'}
-
-def menu_func_import(self, context):
-    self.layout.operator(ImportKCD2ColladaOperator.bl_idname, text=ImportKCD2ColladaOperator.bl_label)
-    self.layout.operator(ImportKCD2SkinOperator.bl_idname, text=ImportKCD2SkinOperator.bl_label)
+modules = [importers, ui]
+classes = [AddonSettings]
 
 def register():
-    bpy.utils.register_class(ImportKCD2ColladaOperator)
-    bpy.utils.register_class(ImportKCD2SkinOperator)
-    bpy.types.TOPBAR_MT_file_import.append(menu_func_import)
+    for module in modules:
+        module.register()
+    for cls in classes:
+        bpy.utils.register_class(cls)
 
 def unregister():
-    bpy.utils.unregister_class(ImportKCD2ColladaOperator)
-    bpy.utils.unregister_class(ImportKCD2SkinOperator)
-    bpy.types.TOPBAR_MT_file_import.remove(menu_func_import)
+    for module in reversed(modules):
+        module.unregister()
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
 
 if __name__ == "__main__":
     register()
---
+++ io_KCD2_Blender_Toolkit/ui.py
@@ -0,0 +1,27 @@
+import bpy
+
+# Base class for the panel
+class UI_BasePanel(bpy.types.Panel):
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "KCD2 Toolkit"
+
+
+class UI_Import(UI_BasePanel):
+    bl_label = "Import KCD2 Asset"
+    bl_idname = "KCD2_PT_Import"
+
+    def draw(self, context):
+        layout = self.layout
+        # Import Collada (.dae)
+        layout.operator("import_scene.kcd2_collada", text="Import COLLADA (.dae)")
+        # Import Skin (.skin)
+        layout.operator("import_scene.kcd2_skin", text="Import Skin (.skin)")
+        # Import CGF (.cgf)
+        layout.operator("import_scene.kcd2_cgf", text="Import CGF (.cgf)")
+
+def register():
+    bpy.utils.register_class(UI_Import)
+
+def unregister():
+    bpy.utils.unregister_class(UI_Import)
